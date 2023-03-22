00c7d5153ebb40aa9340c5b69a8703d4e43a6f53
guilher-phellippe <guilhermephellippe@outlook.com>
2023-03-22 15:03:09 -0300

    testing new features

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3333;
 
 app.use(express.json())
 app.use(cors());
-app.use(route);
+app.use(route)
 
 
 app.listen(PORT, ()=> console.log(`server is running on port ${PORT}`));
