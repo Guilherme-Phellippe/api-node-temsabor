48bb0143a6386ed36ffc4f32318b05e11c8454a5
guilher-phellippe <guilhermephellippe@outlook.com>
2023-03-29 16:20:33 -0300

    clear notification file

--- src/routes/notification/index.ts
+++ src/routes/notification/index.ts
@@ -1,21 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-import { Router } from "express";
+// import { PrismaClient } from "@prisma/client";
+// import { Router } from "express";
 
-const app = Router();
-const prisma = new PrismaClient();
-
-
-app.get("/notification/:id", async (req: any, res: any) => {
-    const { id } = req.params
-
-    const notifications = await prisma.notificationUser.findMany({
-        where:{
-            userId: id
-        }
-    });
-
-    res.status(200).json(notifications)
-})
+// const app = Router();
+// // const prisma = new PrismaClient();
 
 
-export default app
+// export default app
