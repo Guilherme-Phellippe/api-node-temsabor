7db82fd05601a435c31c00140e605d1c2a85f1d8
guilher-phellippe <guilhermephellippe@outlook.com>
2023-03-29 16:21:19 -0300

    clear notification file

--- src/routes/notification/index.ts
+++ src/routes/notification/index.ts
@@ -1,8 +1,8 @@
 // import { PrismaClient } from "@prisma/client";
-// import { Router } from "express";
+import { Router } from "express";
 
-// const app = Router();
+const app = Router();
 // // const prisma = new PrismaClient();
 
 
-// export default app
+export default app
