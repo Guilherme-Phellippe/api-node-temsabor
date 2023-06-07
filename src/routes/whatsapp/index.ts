54742ccd42246978b52f31da531c87a1d1142290
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-07 14:30:31 -0300

    remove whatsapp oficial route

--- src/routes/whatsapp/index.ts
+++ src/routes/whatsapp/index.ts
@@ -1,38 +1,16 @@
 import { Router } from "express";
-import axios from "axios"
 
 const app = Router();
 
 
 app.post("/send-recipe", async (req: any, res: any) => {
-    const messageBody = req.body
-    const token = process.env.TOKEN_WHATSAPP
-
-    try {
-        const response = await axios.post(
-            ' https://graph.facebook.com/v17.0/114494558332190/messages',
-            messageBody,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`,
-                }
-            }
-        );
-
-        res.send(response.data)
-    } catch (error: any) {
-        console.error('Erro ao enviar mensagem:', error.response.data);
-        res.status(404).json(error.response.data)
-    }
+    
 
 })
 
 app.get("/notification-whats", (req, res) => {
 
-    console.log(res)
 
-    res.send("foi")
 })
 
 
