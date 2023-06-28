9cbbe328d792df471ab4e178762bfd25089d64f4
guilher-phellippe <guilhermephellippe@outlook.com>
2023-06-28 16:21:13 -0300

    add all paste here again

---
+++ prisma/migrations/20230628192102_mds/migration.sql
@@ -0,0 +1,177 @@
+-- CreateTable
+CREATE TABLE "Recipe" (
+    "id" TEXT NOT NULL,
+    "categoryId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name_recipe" TEXT NOT NULL,
+    "images_recipe" JSONB[],
+    "videos_recipe" TEXT[],
+    "time" INTEGER NOT NULL,
+    "portion" INTEGER NOT NULL,
+    "ing" TEXT[],
+    "stuffing_ing" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "type_stuffing_ing" TEXT[] DEFAULT ARRAY['Recheio']::TEXT[],
+    "word_key" TEXT[],
+    "prepareMode" TEXT NOT NULL,
+    "type_prepare_mode" TEXT[] DEFAULT ARRAY['Principal']::TEXT[],
+    "nmr_hearts" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "nmr_saved" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "nmr_eyes" INTEGER NOT NULL DEFAULT 0,
+    "votes" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tip" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "name_tip" TEXT NOT NULL,
+    "description_tip" TEXT NOT NULL,
+    "images" JSONB[] DEFAULT ARRAY[]::JSONB[],
+    "votes" TEXT[],
+    "word_key" TEXT[],
+    "nmr_hearts" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "nmr_saved" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "nmr_eyes" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Tip_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Comment" (
+    "id" TEXT NOT NULL,
+    "recipeId" TEXT,
+    "tipId" TEXT,
+    "userId" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "answer" JSONB[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "photo" TEXT NOT NULL DEFAULT 'https://i.ibb.co/JCNSM0R/143086968-2856368904622192-1959732218791162458-n.png',
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "nmr_hearts" INTEGER NOT NULL DEFAULT 0,
+    "nmr_eyes" INTEGER NOT NULL DEFAULT 0,
+    "nmr_saved" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name_category" TEXT NOT NULL,
+    "suggestion" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "isLink" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "NotificationUser" (
+    "notificationId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "NotificationUser_pkey" PRIMARY KEY ("userId","notificationId")
+);
+
+-- CreateTable
+CREATE TABLE "Winner" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "rank" TEXT[],
+    "prizeId" TEXT NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+
+    CONSTRAINT "Winner_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Prize" (
+    "id" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Prize_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "link_shortener" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "origin_link" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "link_shortener_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_category_key" ON "Category"("name_category");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Winner_userId_key" ON "Winner"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "link_shortener_key_key" ON "link_shortener"("key");
+
+-- AddForeignKey
+ALTER TABLE "Recipe" ADD CONSTRAINT "Recipe_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Recipe" ADD CONSTRAINT "Recipe_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Tip" ADD CONSTRAINT "Tip_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_recipeId_fkey" FOREIGN KEY ("recipeId") REFERENCES "Recipe"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_tipId_fkey" FOREIGN KEY ("tipId") REFERENCES "Tip"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NotificationUser" ADD CONSTRAINT "NotificationUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "NotificationUser" ADD CONSTRAINT "NotificationUser_notificationId_fkey" FOREIGN KEY ("notificationId") REFERENCES "Notification"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Winner" ADD CONSTRAINT "Winner_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Winner" ADD CONSTRAINT "Winner_prizeId_fkey" FOREIGN KEY ("prizeId") REFERENCES "Prize"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
