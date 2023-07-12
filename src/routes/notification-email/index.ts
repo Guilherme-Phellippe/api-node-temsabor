07fe4b2b1f20e4a0fbb2a1d982e0a6a56ab171c6
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-12 10:33:05 -0300

    style type email

--- src/routes/notification-email/index.ts
+++ src/routes/notification-email/index.ts
@@ -13,20 +13,23 @@ const transporter = createTransport({
 const app = Router();
 
 app.post("/email/send-recipe", (req, res) => {
-    const { title, link, ings, emails } = req.body
+    const { title, link, ingredients, emails } = req.body
 
-    if (typeof emails !== "object") {
-        res.status(500).json({ Error: "The email inst array type" })
+    if (typeof emails !== "object" || typeof ingredients !== "object") {
+        res.status(500).json({ Error: "The email or ingredients isnt array type" })
         return
     }
 
 
     const emailsToString = emails.join(", ");
+    const ing = ingredients.map((ing: string) => `<li>${ing}</li>`).join("");
+    console.log(ing)
     const html = `
         <div style="width: 100%;height: 100%; display: grid; place-items: center;">
             <h1 style="text-align: center; font-size: 40px; margin: 0 10px;">${title}</h1>
+            <h2 style="text-align: left; font-size: 25px; margin: 0 10px;">INGREDIENTES:</h2>
             <ul style="margin: 0 20px;">
-                ${ings.map((ing: string) => `<li>${ing}</li>`)}
+                ${ing}
              </ul>
             <a href=${link} style="padding: 5px; background-color: #ff6a28; color: white;">VER RECEITA</a>
         </div>
