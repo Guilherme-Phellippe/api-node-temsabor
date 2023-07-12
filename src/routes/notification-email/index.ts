2a4eac549c6b19720bbe19a028b2d4cd13f8199e
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-12 10:26:09 -0300

    style type email

--- src/routes/notification-email/index.ts
+++ src/routes/notification-email/index.ts
@@ -13,25 +13,32 @@ const transporter = createTransport({
 const app = Router();
 
 app.post("/email/send-recipe", (req, res) => {
-    const { title , htmlContent, emails} = req.body
+    const { title, link, ings, emails } = req.body
 
-    console.log(process.env.GMAIL_PASSWORD)
-    
     if (typeof emails !== "object") {
         res.status(500).json({ Error: "The email inst array type" })
         return
     }
-    
+
 
     const emailsToString = emails.join(", ");
+    const html = `
+        <div style="width: 100%;height: 100%; display: grid; place-items: center;">
+            <h1 style="text-align: center; font-size: 40px; margin: 0 10px;">${title}</h1>
+            <ul style="margin: 0 20px;">
+                ${ings.map((ing: string) => `<li>${ing}</li>`)}
+             </ul>
+            <a href=${link} style="padding: 5px; background-color: #ff6a28; color: white;">VER RECEITA</a>
+        </div>
+    `
 
     const info = transporter.sendMail({
         from: 'receitas.temsabor@gmail.com', // sender address
         to: emailsToString, // list of receivers
         subject: title, // Subject line
-        html: htmlContent, // html body
-    }, (err: any)=> {
-        if(!err) res.status(201).json(info)
+        html, // html body
+    }, (err: any) => {
+        if (!err) res.status(201).json(info)
         else res.status(400).json(err)
     });
 })
