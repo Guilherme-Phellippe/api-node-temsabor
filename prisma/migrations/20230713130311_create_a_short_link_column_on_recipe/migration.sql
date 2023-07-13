b219394de70ead37d92de871df4ee4486e586531
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-13 16:57:47 -0300

    trying a fix failed migrate dev

--- prisma/migrations/20230713130311_create_a_short_link_column_on_recipe/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Recipe" ADD COLUMN     "short_link" TEXT NOT NULL DEFAULT '';
--- prisma/migrations/20230713131536_remove_the_column_short_link_on_recipe/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `short_link` on the `Recipe` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Recipe" DROP COLUMN "short_link";
