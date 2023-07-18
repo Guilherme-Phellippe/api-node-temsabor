5b72cb2dba02896290ba7844e33112bf776bb125
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-18 16:28:46 -0300

    add new column on stories -> slug

--- src/routes/stories/index.ts
+++ src/routes/stories/index.ts
@@ -6,26 +6,26 @@ const prisma = new PrismaClient
 
 
 app.get("/stories/", async (req, res) => {
-    const sevenDaysAgo = new Date();
-    sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
-  
-    //push all database 
-    const story = await prisma.stories.findMany({
-      select: {
-        id: true,
-        story_title: true,
-        story_poster_portrait_src: true,
-      },
-      where: {
-        updated_at: {
-          gte: sevenDaysAgo
-        }
+  const sevenDaysAgo = new Date();
+  sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+
+  //push all database 
+  const story: any = await prisma.stories.findMany({
+    select: {
+      id: true,
+      story_title: true,
+      story_poster_portrait_src: true,
+    },
+    where: {
+      updated_at: {
+        gte: sevenDaysAgo
       }
-    });
-  
-    if (story) res.status(200).json(story)
-    else res.status(500)
+    }
   });
 
+  if (story) res.status(200).json(story)
+  else res.status(500)
+});
 
-  export default app
+
+export default app
