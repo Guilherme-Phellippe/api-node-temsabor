ff076c547a540bd3dc7a3afab01b9a7c867c3cb0
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-18 16:32:11 -0300

    add unknow type

--- src/routes/stories/index.ts
+++ src/routes/stories/index.ts
@@ -5,12 +5,20 @@ const app = Router();
 const prisma = new PrismaClient
 
 
+interface storyTypes {
+  id: string,
+  story_title: string,
+  story_poster_portrait_src: string,
+  updated_at: string,
+}
+
+
 app.get("/stories/", async (req, res) => {
   const sevenDaysAgo = new Date();
   sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
 
   //push all database 
-  const story: any = await prisma.stories.findMany({
+  const story = await prisma.stories.findMany({
     select: {
       id: true,
       story_title: true,
@@ -21,7 +29,7 @@ app.get("/stories/", async (req, res) => {
         gte: sevenDaysAgo
       }
     }
-  });
+  }) as unknown;
 
   if (story) res.status(200).json(story)
   else res.status(500)
