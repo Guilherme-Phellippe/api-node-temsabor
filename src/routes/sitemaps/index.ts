4d822ab78ce2011edfd4e6a22cdf9cbf1a83cf93
guilher-phellippe <guilhermephellippe@outlook.com>
2023-07-26 12:00:54 -0300

    change xml to rss in sitemaps

--- src/routes/sitemaps/index.ts
+++ src/routes/sitemaps/index.ts
@@ -40,42 +40,79 @@ app.get("/recipes-news.xml", async (req, res) => {
         select: {
             slug: true,
             name_recipe: true,
+            ing: true,
             word_key: true,
+            user: {
+                select: {
+                    name: true,
+                }
+            },
+            category: {
+                select: {
+                    name_category: true,
+                }
+            },
+            comments: {
+                select: {
+                    comment: true
+                }
+            },
             createdAt: true
         }
     });
 
-    const urlset = xmlbuilder.create('urlset', { version: '1.0', encoding: 'UTF-8' });
-    urlset.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-    urlset.att('xmlns:news', 'http://www.google.com/schemas/sitemap-news/0.9');
+
+    const root = xmlbuilder.create('rss', { version: '1.0', encoding: 'UTF-8' });
+    root.attribute('xmlns:content', 'http://purl.org/rss/1.0/modules/content/');
+    root.attribute('xmlns:wfw', 'http://wellformedweb.org/CommentAPI/');
+    root.attribute('xmlns:dc', 'http://purl.org/dc/elements/1.1/');
+    root.attribute('xmlns:atom', 'http://www.w3.org/2005/Atom');
+    root.attribute('xmlns:sy', 'http://purl.org/rss/1.0/modules/syndication/');
+    root.attribute('xmlns:slash', 'http://purl.org/rss/1.0/modules/slash/');
+    root.attribute('version', '2.0');
+
+    const channel = root.ele('channel');
+    channel.ele('title', 'Tem sabor | Receitas oficiais');
+    channel.ele('atom:link', { href: 'https://api.temsabor.blog/recipes-news.xml', rel: 'self', type: 'application/rss+xml' });
+    channel.ele('link');
+    channel.ele('description', 'Aqui no blog da Tem Sabor, você irá descobrir uma variedade de receitas deliciosas e também compartilhar as suas receitas, nosso blog é uma rede social, onde todos tem espaço para interagir com suas receitas.');
+    channel.ele('lastBuildDate', 'Thu, 25 Jul 2023 10:08:05 +0000');
+    channel.ele('language', 'pt-BR');
+    channel.ele('sy:updatePeriod', 'hourly');
+    channel.ele('sy:updateFrequency', '1');
+
+    const image = channel.ele('image');
+    image.ele('url', 'https://i.ibb.co/G5ZcQTs/Design-sem-nome-Photo-Room-png-Photo-Room.png');
+    image.ele('title', 'Tem sabor receitas oficiais');
+    image.ele('link');
+    image.ele('width', '32');
+    image.ele('height', '32');
 
 
     recipes.forEach(recipe => {
-        const news = urlset.ele('url');
-        news.ele('loc', `https://temsabor.blog/receitas/${recipe.slug}`);
-        news.ele('lastmod', moment(recipe.createdAt).toISOString());
-
-        const newsData = news.ele('news:news');
-        newsData.ele('news:publication_date', moment(recipe.createdAt).toISOString());
-        newsData.ele('news:title').dat(recipe.name_recipe);
-        newsData.ele('news:keywords').dat(`Notícia, Receitas, Google News, ${!!recipe.word_key.length && recipe.word_key[0]}`);
-        const publicationElement = newsData.ele('news:publication')
-        publicationElement.ele('news:name', 'Tem sabor receitas');
-        publicationElement.ele('news:language', "pt-br");
+        const countComment = recipe.comments.length
+        const rssRecipe = channel.ele('item');
+        rssRecipe.ele('title', recipe.name_recipe);
+        rssRecipe.ele('link', `https://temsabor.blog/receitas/${recipe.slug}`);
+        rssRecipe.ele('dc:creator', recipe.user.name);
+        rssRecipe.ele('pubDate', moment(recipe.createdAt).toISOString());
+        rssRecipe.ele('category', recipe.category.name_category);
+        rssRecipe.ele('guid', { isPermaLink: 'false' }, recipe.slug);
+        rssRecipe.ele('description', { 'content:encoded': "Você precisa conhecer essa receita! criamos com ingredientes selecionados e medidos para um sabor irresistível." });
+        rssRecipe.ele('content:encoded', recipe.ing);
+        rssRecipe.ele('slash:comments', countComment);
     })
 
-    const xml = urlset.end({ pretty: true });
-
-    // Envie o sitemap.xml como resposta
-    res.set('Content-Type', 'application/xml');
-    res.send(xml);
+    const rssFeed = root.end({ pretty: true });
+    res.set('Content-Type', 'application/rss+xml; charset=utf-8');
+    res.send(rssFeed);
 })
 
 
 app.get("/tips-news.xml", async (req, res) => {
     const tips = await prisma.tip.findMany({
         select: {
-            id:true,
+            id: true,
             name_tip: true,
             word_key: true,
             createdAt: true
@@ -86,19 +123,18 @@ app.get("/tips-news.xml", async (req, res) => {
     urlset.att('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
     urlset.att('xmlns:news', 'http://www.google.com/schemas/sitemap-news/0.9');
 
-
     tips.forEach(tip => {
         const news = urlset.ele('url');
         news.ele('loc', `https://temsabor.blog/tip/${tip.name_tip}/${tip.id}`);
         news.ele('lastmod', moment(tip.createdAt).toISOString());
 
         const newsData = news.ele('news:news');
-        newsData.ele('news:publication_date', moment(tip.createdAt).toISOString());
-        newsData.ele('news:title').dat(tip.name_tip);
-        newsData.ele('news:keywords').dat(`Notícia, Receitas, Google News, ${!!tip.word_key.length && tip.word_key[0]}`);
         const publicationElement = newsData.ele('news:publication')
         publicationElement.ele('news:name', 'Tem sabor receitas');
         publicationElement.ele('news:language', "pt-br");
+        newsData.ele('news:publication_date', moment(tip.createdAt).toISOString());
+        newsData.ele('news:title').dat(tip.name_tip);
+        newsData.ele('news:keywords').dat(`Notícia, Receitas, Google News, ${!!tip.word_key.length && tip.word_key[0]}`);
     })
 
     const xml = urlset.end({ pretty: true });
