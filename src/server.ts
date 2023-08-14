e4570bd3c84093cb78d7e206e2bb5aa555d8f0c3
Guilherme Phellippe <guilhermephellippe@outlook.com>
2023-08-14 15:42:39 -0300

    add compression to routes

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import route from './routes.js';
 import dotenv from 'dotenv';
+import compression from "compression";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-app.use(cors({
-    origin: ["https://temsabor.blog", "http://localhost:3000", "http://127.0.0.0:3000/", "http://localhost:5173", "http://127.0.0.0:5173/"]
-}));
-
+app.use(cors({origin: ["https://temsabor.blog", "http://localhost:3000", "http://127.0.0.0:3000/", "http://localhost:5173", "http://127.0.0.0:5173/"]}));
+app.use(compression())
 app.use(express.json())
 app.use(route)
 
